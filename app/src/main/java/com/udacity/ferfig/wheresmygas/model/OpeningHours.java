f3fb338d104062554127c90f20e1be575ab233c0
Fernando F <ferfig.apps@gmail.com>
2018-07-20 23:57:21 +0100

    Added POJO classes

---
+++ app/src/main/java/com/udacity/ferfig/wheresmygas/model/OpeningHours.java
@@ -0,0 +1,56 @@
+
+package com.udacity.ferfig.wheresmygas.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class OpeningHours implements Parcelable
+{
+
+    @SerializedName("open_now")
+    @Expose
+    private Boolean openNow;
+    public final static Parcelable.Creator<OpeningHours> CREATOR = new Creator<OpeningHours>() {
+
+
+        @SuppressWarnings({
+            "unchecked"
+        })
+        public OpeningHours createFromParcel(Parcel in) {
+            return new OpeningHours(in);
+        }
+
+        public OpeningHours[] newArray(int size) {
+            return (new OpeningHours[size]);
+        }
+
+    }
+    ;
+
+    protected OpeningHours(Parcel in) {
+        this.openNow = ((Boolean) in.readValue((Boolean.class.getClassLoader())));
+    }
+
+    public OpeningHours() {
+    }
+
+    public Boolean getOpenNow() {
+        return openNow;
+    }
+
+    public void setOpenNow(Boolean openNow) {
+        this.openNow = openNow;
+    }
+
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(openNow);
+    }
+
+    public int describeContents() {
+        return  0;
+    }
+
+}
